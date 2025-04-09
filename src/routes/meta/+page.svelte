e1ff65998a73ffb5fd63a2d9249ce2d37014cb14
Leighton Estrada <lestradar@uni.edu.pe>
2025-04-08 21:55:16 -0300

    Lab 7 upd2 step1.2

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -3,10 +3,47 @@ import * as d3 from "d3";
 import { onMount } from "svelte";
 
 let data = [];
+let commits = [];
 
 onMount(async () => {
-	data = await d3.csv("/loc.csv");
+	data = await d3.csv("/loc.csv", row => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + "T00:00" + row.timezone),
+    datetime: new Date(row.datetime)
+	}));
+	// console.log(data)  //Step 1.1
+
+	//Step 1.2
+	commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
+    let first = lines[0];
+    let {author, date, time, timezone, datetime} = first;
+    let ret = {
+        id: commit,
+        url: "https://github.com/Leandr0ER/REPO/commit/" + commit,
+        author, date, time, timezone, datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length
+    };
+
+    // Like ret.lines = lines
+    // but non-enumerable so it doesn’t show up in JSON.stringify
+    Object.defineProperty(ret, "lines", {
+        value: lines,
+        configurable: true,
+        writable: true,
+        enumerable: false,
+    });
+
+    return ret;
+	});
+
+	console.log(commits)
 });
+
+
 
 </script>
 <svelte:head>
